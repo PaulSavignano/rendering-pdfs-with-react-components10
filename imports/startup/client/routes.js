1ee3868f594c762893a887f99994922bd8c69bf6
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-01 15:39:01 -0700

    Added Documents component.

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -9,6 +9,7 @@ import { NotFound } from '../../ui/pages/not-found';
 import { RecoverPassword } from '../../ui/pages/recover-password';
 import { ResetPassword } from '../../ui/pages/reset-password';
 import { Signup } from '../../ui/pages/signup';
+import { Documents } from '../../ui/pages/documents'
 
 const requireAuth = (nextState, replace) => {
   if (!Meteor.loggingIn() && !Meteor.userId()) {
@@ -24,6 +25,7 @@ Meteor.startup(() => {
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
         <IndexRoute name="index" component={ Index } onEnter={ requireAuth } />
+        <Route name="documents" path="/documents" component={ Documents } />
         <Route name="login" path="/login" component={ Login } />
         <Route name="recover-password" path="/recover-password" component={ RecoverPassword } />
         <Route name="reset-password" path="/reset-password/:token" component={ ResetPassword } />
