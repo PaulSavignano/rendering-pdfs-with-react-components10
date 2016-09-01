1ea271a99ab10f7fb03138e89ae611e2fddb66ea
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-01 15:14:13 -0700

    Added Documents collection.

---
+++ imports/ui/components/authenticated-navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { browserHistory } from 'react-router';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+
+const handleLogout = () => Meteor.logout(() => browserHistory.push('/login'));
+
+const userName = () => {
+  const user = Meteor.user();
+  const name = user && user.profile ? user.profile.name : '';
+  return user ? `${name.first} ${name.last}` : '';
+};
+
+export const AuthenticatedNavigation = () => (
+  <div>
+    <Nav>
+      <IndexLinkContainer to="/">
+        <NavItem eventKey={ 1 } href="/">Index</NavItem>
+      </IndexLinkContainer>
+    </Nav>
+    <Nav pullRight>
+      <NavDropdown eventKey={ 2 } title={ userName() } id="basic-nav-dropdown">
+        <MenuItem eventKey={ 2.1 } onClick={ handleLogout }>Logout</MenuItem>
+      </NavDropdown>
+    </Nav>
+  </div>
+);
